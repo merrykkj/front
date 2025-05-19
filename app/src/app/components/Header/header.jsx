b2decb6008fc54aae549a3ab372b0cf85013b3c8
VITORIA PEREIRA DE LUCENA <24251125@educ123.sp.senai.br>
2025-05-19 11:20:33 -0300

    novo commit

---
+++ app/src/app/components/Header/header.jsx
@@ -0,0 +1,13 @@
+export default function Header(){
+    return(
+        <>
+        <nav>
+            <ul className="text-black bg-blue-300 flex gap-6">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Fale Conosco</a></li>
+                <li><a href="#">Especialistas</a></li>
+            </ul>
+        </nav>
+        </>
+    )
+}
